17093a889ba2bfef9418e6a62e7798cb2e72566b
RikaMersita <rikamersita@gmail.com>
2020-11-27 14:12:24 +0700

    TA

---
+++ app/src/main/java/com/rikamersita/catatankeuanganpribadi/MenuUtamaActivity.java
@@ -0,0 +1,16 @@
+package com.rikamersita.catatankeuanganpribadi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuUtamaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_utama);
+
+        getSupportActionBar().hide();
+    }
+}
--- app/src/main/java/com/rikamersita/catatankeuanganpribadi/SignUpActivity.java
+++ app/src/main/java/com/rikamersita/catatankeuanganpribadi/SignUpActivity.java
@@ -6,18 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up);
 
         getSupportActionBar().hide();
     }
 
-    public void signup(View view) {
-        Intent daftar = new Intent(this,SignUpActivity.class);
-        startActivity(daftar);
+    public void signin(View view) {
+        Intent login = new Intent(this,MainActivity.class);
+        startActivity(login);
     }
 }
