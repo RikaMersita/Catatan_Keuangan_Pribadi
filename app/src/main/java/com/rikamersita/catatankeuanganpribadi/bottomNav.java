78e09adb8409a11b018c9eb3fc5ca10f9a34d078
RikaMersita <rikamersita@gmail.com>
2020-12-02 10:46:10 +0700

    TA

--- app/src/main/java/com/rikamersita/catatankeuanganpribadi/bottomNav.java
+++ app/src/main/java/com/rikamersita/catatankeuanganpribadi/bottomNav.java
@@ -16,11 +16,12 @@ public class bottomNav extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_nav);
+        getSupportActionBar().hide();
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_masuk, R.id.navigation_keluar, R.id.navigation_akhir)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
